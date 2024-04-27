d26ab01e839d3bfed14a6dda4826f8b6e5a6ea37
frankkulak <fs.kulak@gmail.com>
2024-04-26 21:08:51 -0400

    Deploy.

---
+++ v1.0.0/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAE33OPQ+CMBAG4P/Smag0wQ9GArODg4NxaMoZLpaD0DOxGv+7HUyASrree/e8d3kLhieLXBRIanAl6K6GQSSiV9z4sTbKWrDrWbxquDV+545UizyV+08SOBVFnV8cc0pHqkV9whcUDzQzjF3vqf+NuSc3h12ayah5Rm6OPWNHduSRGIab0osdk4ugLttOqiqqURGBteHbY7L87vULGnDv1JQBAAA="
